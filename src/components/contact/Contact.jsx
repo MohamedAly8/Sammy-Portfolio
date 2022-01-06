8e4f7fb1540cb1f0fd7b9ffa112cd2930f32fac4
Mohamed Aly <mohamedaly@Mohameds-MacBook-Pro.local>
2022-01-06 02:31:37 -0500

    Added EmailJS compatibility for Contact Me form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,28 @@
 import "./contact.css"
-import { useRef } from "react";
+import { useRef , useState } from "react";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
 
-    const formRef = useRef()
+    const formRef = useRef();
+    const [done, setDone] = useState(false);
+
+    const Style = { color: 'red'};
+    const thanks_message = <p style={ Style}>Thank you for your email, I will respond shortly</p>
+
 
     const handleSubmit = (e)=>{
         e.preventDefault()
+
+        // For use with EmailJS
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formRef.current, 'YOUR_USER_ID')
+        .then((result) => {
+            console.log(result.text);
+            setDone(true)
+        }, (error) => {
+            console.log(error.text);
+            
+        });
     }
     return (
         
@@ -23,14 +39,16 @@ function Contact() {
             Contact Me
         </div>
 
-        <form ref={formRef} onSubmit={ handleSubmit }>      
+        <form ref={formRef} onSubmit={ handleSubmit }>
+           
         <input name="user_name" type="text" className="name" placeholder="Name" />   
         <input name="email" type="email" className="email" placeholder="Email" />
         <input name="user_subject" type="text" className="user_subject" placeholder="Subject" />
         <textarea rows="5" name="text" className="message" placeholder="Message"></textarea>
         <button className="submit">Submit</button>
+        {done && thanks_message}
+        
         </form>
-
 
         </div>
                     
